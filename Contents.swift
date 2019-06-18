c37b48308ff9a697de7e72700d98f0288aaf284f
shannonbee <shannonbenyamin@gmail.com>
2019-06-18 16:41:55 -0400

    first commit

---
+++ Contents.swift
@@ -0,0 +1,143 @@
+//class FacebookProfile {
+//    // we are going to design this class first before we even touch the computer
+//    //attributes section: what a FB is
+//    var bio: String
+//    var friendCount:Int
+//    var userName:String
+//    var birthday: String
+//    var relationStatus: String
+//    var friendList: [String]
+//
+//    //preselected Options for RelationStatus
+//    let optionOne = "Single"
+//    let optionTwo = "It's Complicated"
+//    let OptionThree = "Married"
+//    let OptionFour = "Divorced"
+//    //fill in the rest of your options
+//    //functions that support our user using
+//    //FB profile
+//    //function that lets user update their bio
+//    func setBio (userInput: String) {
+//        bio = userInput
+//    }
+//    //function that lets user update their FB name
+//    func setUserName (userInput: String) {
+//        userName = userInput
+//    }
+//    func setFriend() {
+//        var friendsNum =
+//        friendList.count
+//        friendCount = friendsNum
+//        }
+//    func setBirthday (userInput: String) {
+//        birthday = userInput
+////    }
+////
+//////function that updates the profiles
+//////relationship status
+//////think about what goes into this function: what data type should userinput be?
+////
+////    func setRelationships (userInput: String) {
+////        if userInput == optionOne {
+////            relationStatus = optionOne
+////        } else if userInput == optionTwo {
+////            relationStatus == optionTwo
+////        } else if userInput == OptionThree {
+////            relationStatus == OptionThree }
+////        else if userInput == OptionFour {
+////            relationStatus == OptionFour
+////        }
+////        else if userInput == op
+//
+//        func setStatus(userInput: Int) {
+//            let status = [1:optionOne, 2:optionTwo, 3:optionThree, 4:OptionFour]
+//            for option in status {
+//                if option.key == userInput {
+//                    relationStatus = option.value
+//                }
+//            }
+//        }
+////
+////
+////
+////        var relationStatus = userInput
+////
+////print ("single")
+////        else if relationStatus == "It's Complicated"
+////print "It's Complicated"
+////else if relationStatus = "Married"
+////
+//
+////wrapper functions: lets the user check their profile info
+//func checkBio () -> String {
+//
+//    func checkFriendCound() -> Int {
+//        return friendCount
+//    }
+//// write the rest of the wrapper functions for: userName, Birthday, Relationshipstatus
+//
+//}
+//        func checkuserName() -> String {
+//            return userName
+//
+//}
+//        func checkBirthday() -> String {
+//            return birthday
+//
+//}
+//        func checkRelationStatus() -> String {
+//            return relationStatus
+//            }
+//
+////init section init -> init -> initializer
+////tells the computer how to built an object of the class
+//        init() {
+//            bio = ""
+//            friendCount = 0
+//            userName = ""
+//            birthday = ""
+//            relationStatus = optionTwo
+//            friendList = [String]()
+//            }
+//                 }
+//
+////testing FB profiles
+//var cassieProfile = FacebookProfile ()
+//        cassieProfile.setUserName (userInput: "Cassie")
+//        cassieProfile.setRelations(userInput: 2)
+//cassieProfile. checkUserName()
+//
+//cassieProfile.chceckUserName()
+//cassieProfile.chceckRelationStatus()
+
+//class BankAccount {
+//var name:String
+//var accountBalance:Double
+//var transferNumber:Int
+//var Password: String
+//
+//    func setname (userInput: String) {
+//
+//        name=userInput
+//}
+//    func setaccountBalance (userinput: Double) {
+//        accountBalance = userinput
+//
+//    }
+//    func settransferNumber (userInput:Int) {
+//        transferNumber = userInput
+//        }
+//    func setPassword (userInput: String) {
+//        Password=userInput
+//        }
+//    func makeWithdrawal (withdrawalAmount:Double) {
+//        accountBalance = accountBalance - withdrawalAmount
+//    }
+//    func makeDeposit (depositAmount: Double){
+//        accountBalance = accountBalance +
+//    }
+//}
+var animals = ["red panda", "penguin", "polar bear" ]
+for index in 0...
+
+func
